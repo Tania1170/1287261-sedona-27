a4232b51d1f3087d8bf09dcdfd0309b06a53edb0
Tania1170 <60141570+Tania1170@users.noreply.github.com>
2020-03-18 22:14:33 +0200

    завершение верстки

---
+++ js/script-min.js
@@ -0,0 +1,1 @@
+var popup=document.querySelector(".reservation");var close=document.querySelector(".button-search");close.addEventListener("click",function(evt){evt.preventDefault();popup.classList.toggle("modal-close");});
